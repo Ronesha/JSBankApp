315589aece3c50ae42f0054d905acfda70f50a9b
Reyna Egson <ro.egson@gmail.com>
2018-03-31 19:23:06 -0600

    completed

--- app2.js
+++ app2.js
@@ -9,7 +9,7 @@ function bankAccountEntry(desc, value, type){
 //function to display to UI
 function displayAccount(){
    for(var [i] in account){
-      if(account[i].type === 'deposit'){
+          if(account[i].type === 'deposit'){
          let parent = document.querySelector('.depositBlock');
          let template = ` 
                             <div class="item_list" id="deposit">
@@ -64,6 +64,20 @@ function displayTotal(){
    $('.tot').html(accountTotal())
 }
 
+//create an alert if account is overdrawn
+function overDrawn(){
+    //change the color of the account balance to red
+    $('.tot').css('color','red');
+    //add html to span tag 
+    $('.alert').html('<h4>Account Overdrawn!</h4>')
+    $('.alert').css('color', 'red');
+}
+
+//if account is not overdrawn
+function accountBalanced(){
+  $('.tot').css('color', 'black');
+  $('.alert').html('');
+}
 
 //create a function to push a new instance of the bankAccount object into the Account array
 function createNewListing(desc, value, type){
@@ -82,7 +96,12 @@ function accountTotal(){
          total -= parseInt(account[i].value);
       }
       if(total < 0){
-         console.log('Account Overdrawn!');
+        
+         //run this function
+         overDrawn();
+      }
+      else{
+         accountBalanced();
       }
    }
    return total;
@@ -93,11 +112,18 @@ function accountTotal(){
    let desc = document.querySelector('.debName').value;
    let value = document.querySelector('.debCost').value;
    let type = 'deposit'; 
+
+   if(desc === '' || value === ''){
+          validate();
+   }
+   else{
    e.preventDefault();
    createNewListing(desc, value, type);
    accountTotal();
    displayAccount();
    displayTotal();
+   clearFields();
+   }
  })
 
 // withdraw event listener
@@ -106,12 +132,36 @@ $('.takeFrom').on('click', function(e){
    let desc = document.querySelector('.widName').value;
    let value = document.querySelector('.widCost').value;
    let type = 'withdraw'; 
+   if(desc === '' || value === ''){
+    validate();
+}
+else{
    e.preventDefault();
    createNewListing(desc, value, type);
    accountTotal();
    displayAccount();
    displayTotal();
+   clearFields();
+}
 })
 
+//clear text fields
+function clearFields(){
+ document.querySelector('#dName').value = '';
+ document.querySelector('#dCost').value = '';
+ document.querySelector('#wName').value = '';
+ document.querySelector('#wCost').value = '';
+}
 
+//form validation
+function validate(){
+  $('.alert').css('color', 'red');
+  $('.alert').html('Please enter all fields');
+}
 
+//form validation text input
+// Function to check letters and numbers
+
+function validateText(){
+
+}
